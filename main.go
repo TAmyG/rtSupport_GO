79e0d53f39572385159ce85901b3cff63387a1fb
TAmyG <tamy.vivas@gmail.com>
2017-09-03 15:32:00 -0600

    type: feat, servidor HTTP

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":4000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hola munod")
 }
